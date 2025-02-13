d89970f170d030d51a7803ce940853d45cef2d95
Damien Flutre <144629417+DamiennFl@users.noreply.github.com>
2025-02-12 21:29:03 -0800

    LinearFormation constructor and Update

--- Ascension/Enemies/EnemyFormation/EnemyFormation.cs
+++ Ascension/Enemies/EnemyFormation/EnemyFormation.cs
@@ -18,6 +18,6 @@ namespace Ascension.Enemies.EnemyFormation
 
         public abstract void Update(GameTime gameTime);
 
-
+        // TODO: Remove dead enemies
     }
 }
--- Ascension/Enemies/EnemyFormation/LinearFormation.cs
+++ Ascension/Enemies/EnemyFormation/LinearFormation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,48 @@ namespace Ascension.Enemies.EnemyFormation
     /// <summary>
     /// LinearFormation spawns enemies in a linear formation.
     /// </summary>
-    internal class LinearFormation
+    internal class LinearFormation : EnemyFormation
     {
         // TODO: Write linear formation
+        private float spawnDelay;
+        private float timeSinceLastSpawn;
+        private int numEnemies;
+        private int enemiesSpawned;
+        private Vector2 enemyVelocity;
+        private float enemySpacing;
+        private EnemyFactory enemyFactory;
+
+        public LinearFormation(Vector2 formationPosition, int numEnemies, float spawnDelay, Vector2 enemyVelocity, float enemySpacing, EnemyFactory factory) : base(formationPosition)
+        {
+            this.numEnemies = numEnemies;
+            this.spawnDelay = spawnDelay;
+            this.timeSinceLastSpawn = 0;
+            this.enemiesSpawned = 0;
+            this.enemyVelocity = enemyVelocity;
+            this.enemySpacing = enemySpacing;
+            this.enemyFactory = factory;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.timeSinceLastSpawn += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (this.enemiesSpawned < this.numEnemies && this.timeSinceLastSpawn >= this.spawnDelay)
+            {
+                // Calculate position for the new enemy.
+                Vector2 enemyPosition = this.FormationStartPosition + new Vector2(this.enemiesSpawned * this.enemySpacing, 0);
+                Enemy newEnemy = this.enemyFactory.CreateEnemy(enemyPosition, "EnemyA");
+                // newEnemy.AddMovementComponent(new LinearMovement(_enemyVelocity));
+                this.enemies.Add(newEnemy);
+                this.enemiesSpawned++;
+                this.timeSinceLastSpawn = 0;
+            }
+
+            // Update all spawned enemies
+            foreach (var enemy in this.enemies)
+            {
+                enemy.Update(gameTime);
+            }
+        }
     }
 }
